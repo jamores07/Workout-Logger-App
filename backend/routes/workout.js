fe9c890e0afdaa106a4a6ad6996f236a3b164fb8
John Amores <kayak.stated_0b@icloud.com>
2023-12-12 00:43:43 +0000

    setup connection to Mongo DB and added model schema

--- backend/routes/workout.js
+++ backend/routes/workout.js
@@ -1,4 +1,5 @@
 const express = require('express')
+const Workout = require('../models/workoutModel')
 
 const router = express.Router()
 
@@ -8,8 +9,15 @@ router.get('/', (req, res) => {
 })
 
 // POST a new workout
-router.post('/', (req, res) => {
-    res.json({mssg: 'POST a new workout'})
+router.post('/', async (req, res) => {
+    const {title, load, reps} = req.body
+
+    try{
+        const workout = await Workout.create({title, load, reps})
+        res.status(200).json(workout)
+    }catch(error) {
+        res.status(400).json({error : error.message})
+    }
 })
 
 // GET a single workout
--- backend/server.js
+++ backend/server.js
@@ -22,11 +22,14 @@ app.use( '/api/workouts', workoutRoutes)
 
 //connect to DB
 mongoose.connect(process.env.MONGO_URI)
- .then (() => {
-    console.log('connected to database')
- })
-
-//listen for requests
-app.listen(process.env.PORT, () => {
-    console.log('listening on', process.env.PORT)
+ .then(() => {
+    //listen for requests
+    app.listen(process.env.PORT, () => {
+        console.log(' connected to DB & listening on', process.env.PORT)
+    })
 })
+ .catch((err) => {
+    console.log(err)
+}) 
+
+
